af21d73e155ffe21fcbcc36aad60fb69ae12ed7f
StefaniaSgreva <stefaniasgreva@gmail.com>
2022-12-31 17:42:53 +0100

    basic layout

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,20 @@
 "use strict";
+/*------------------------------------
+    OPERAZIONI PRELIMINARI
+------------------------------------*/
+const changeImageBtn = document.querySelector('#change-image');
+const changeTextBtn = document.querySelector('#change-text');
+
+/*------------------------------------
+    OPERAZIONI DI AVVIO (MAIN)
+------------------------------------*/
+
+/*------------------------------------
+    EVENTI DINAMICI
+------------------------------------*/
+
+changeImageBtn.addEventListener('click', );
+
+/*------------------------------------
+    FUNZIONI
+------------------------------------*/
