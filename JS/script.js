e05bdecce3415643d198b5e93e54986198824b96
StefaniaSgreva <stefaniasgreva@gmail.com>
2023-01-01 12:38:34 +0100

    + js functionalities

--- JS/script.js
+++ JS/script.js
@@ -1,20 +1,75 @@
 "use strict";
+
 /*------------------------------------
     OPERAZIONI PRELIMINARI
 ------------------------------------*/
+
 const changeImageBtn = document.querySelector('#change-image');
 const changeTextBtn = document.querySelector('#change-text');
-
-/*------------------------------------
-    OPERAZIONI DI AVVIO (MAIN)
-------------------------------------*/
-
-/*------------------------------------
-    EVENTI DINAMICI
-------------------------------------*/
-
-changeImageBtn.addEventListener('click', );
+const previewImage = document.querySelector('#preview-img');
+const previewText = document.querySelector('#preview-txt');
+const previewSender = document.querySelector('.preview-sender');
+const senderInput = document.querySelector('#sender');
+const downloadBtn = document.querySelector('#download');
+const previewArea = document.querySelector('.preview');
+//variabili di funzione
+const assetsCount = 4;
+let currentImageIndex = 1;
+let currentTextIndex = 1;
 
 /*------------------------------------
     FUNZIONI
 ------------------------------------*/
+
+function changeImage(){
+    currentImageIndex++;
+
+    if(currentImageIndex > assetsCount){
+        currentImageIndex = 1;
+    }
+
+    previewImage.src = `./images/img_${currentImageIndex}.png`;
+}
+
+function changeText(){
+    currentTextIndex++;
+
+    if(currentTextIndex > assetsCount){
+        currentTextIndex = 1;
+    }
+
+    previewText.src = `./images/txt_${currentTextIndex}.png`;
+}
+
+function updateSender(){
+    const txt = senderInput.value;
+
+    if(txt.trim().length > 0){
+         previewSender.innerText = txt;
+    } else{
+        previewSender.innerText = 'Augurissimi';
+    }
+
+}
+
+function downloadImg(){
+    htmlToImage.toJpeg(previewArea)
+  .then(function (dataUrl) {
+    var link = document.createElement('a');
+    link.download = 'my-image-name.jpeg';
+    link.href = dataUrl;
+    link.click();
+  });
+}
+ 
+/*------------------------------------
+    EVENTI DINAMICI
+------------------------------------*/
+
+changeImageBtn.addEventListener('click', changeImage);
+changeTextBtn.addEventListener('click', changeText);
+senderInput.addEventListener('input', updateSender);
+downloadBtn.addEventListener('click', downloadImg);
+
+
+
